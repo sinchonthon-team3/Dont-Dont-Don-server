5636021f533e16a3ecfd2a414e2685a0ab06f43e
JTH <nex_david@naver.com>
2023-08-25 22:58:49 +0900

    :sparkles: random1 view complete

--- random1/urls.py
+++ random1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import UserRegisterView, CustomTokenObtainPairView
+
 
 router = DefaultRouter()
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-register'),            # 회원가입 Api 문서 Ok
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # 로그인 Api 문서 Ok
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),       # access token 리프레쉬 Api 문서 Ok
+
+    path('get-random-money/', get_random_money, name='get-random-money')        # 1번 뷰
 ]
