e92326ade60dad4770397aba977650792970aa0c
JTH <nex_david@naver.com>
2023-08-26 00:53:16 +0900

    :zap: DB settings complete

--- config/settings.py
+++ config/settings.py
@@ -151,13 +151,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# 데이터베이스는 AWS RDS Mysql 사용 했습니다.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+		'NAME': get_env_variable('NAME'),
+        'USER': get_env_variable('USER'),
+        'PASSWORD': get_env_variable('PASSWORD'),
+        'HOST': get_env_variable('HOST'),
+        'PORT': get_env_variable('PORT'),
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
-}
-
+}    
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -193,7 +200,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
