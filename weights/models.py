bb6c48b51825dd5837064a5e158d22235330c872
HyunWoo <kimjd766@gmail.com>
2023-08-25 22:06:17 +0900

    feat: weight 모델 수정

--- weights/models.py
+++ weights/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Weight(models.Model):
     weight_name = models.CharField(max_length=20)
-    positive = models.BooleanField()
     is_travel = models.BooleanField()
+    amount = models.IntegerField()
